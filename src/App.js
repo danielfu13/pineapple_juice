4d64f54f32e81ebadfc28e4ea9f2acab2f8e249e
YonisHussein <yonismhussein@gmail.com>
2022-07-29 14:09:38 -0400

    create-exercise component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import ExerciseList from "./components/exercises-list.component";
@@ -10,14 +10,16 @@ import CreateExercise from "./components/create-exercise.component"
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <br/>
-      <Routes>
-        <Route path="/" exact element ={<ExerciseList/>}/>
-        <Route path="/edit/:id" element ={<EditExercise/>}/>
-        <Route path="/create" element ={<CreateExercise/>}/>
-        <Route path="/user"  element ={<CreateUser/>}/>
-      </Routes>
+      <div className="container">
+        <Navbar/>
+        <br/>
+        <Routes>
+          <Route path="/" exact element ={<ExerciseList/>}/>
+          <Route path="/edit/:id" element ={<EditExercise/>}/>
+          <Route path="/create" element ={<CreateExercise/>}/>
+          <Route path="/user"  element ={<CreateUser/>}/>
+        </Routes>
+      </div>
     </Router>
     //module 21, activity 20, solved
   );
