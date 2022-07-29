278fb3eb5c464be8ece640074d358e903fed05c6
YonisHussein <yonismhussein@gmail.com>
2022-07-29 14:16:54 -0400

    create-user added onChange

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 
 
 export default class CreateUsers extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUserName = this.onChangeUserName.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        
+        this.state = {
+            username: '',
+            
+        }
+    }
+
+    onChangeUserName(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
     render() {
         return (
             <div>
